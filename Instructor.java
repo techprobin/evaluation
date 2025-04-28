a7d26fd96ab244147b513dfb6f7c15ca243448cb
techprobin <153546094+techprobin@users.noreply.github.com>
2025-04-28 14:32:07 +0530

    Create Instructor.java
    Defining Relation between Batch, Class, Instructor and Learner in EdTech Ecosystem

---
+++ Instructor.java
@@ -0,0 +1,23 @@
+package org.example.evaluations.evaluation.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+    @Id
+    private UUID id;
+
+    @ManyToMany(mappedBy = "instructors")
+    private Set<Batch> batches=new HashSet<>();
+
+    private String name;
+
+    private String email;
+}
